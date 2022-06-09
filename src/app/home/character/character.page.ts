23f7d46682f1fe9013a60d09f935b6b747111549
ju_ficher <juliana.ficher@hotmail.com>
2022-06-09 20:44:00 -0300

    correção da página character

--- src/app/home/character/character.page.ts
+++ src/app/home/character/character.page.ts
@@ -9,73 +9,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CharacterPage {
 
+  public comics : any=[];
+  
   public character = {
     id: 0,
-    comics: {
-        available: 0, 
-        collectionURI: '', 
-        items: [], 
-        returned: 0
-    },
-    description: '',
-    events: {
-        available: 0, 
-        collectionURI: '', 
-        items: [], 
-        returned: 0
-    },
-    modified: '',
-    name: '',
-    resourceURI: '',
-    series: {
-        available: 0, 
-        collectionURI: '', 
-        items: [], 
-        returned: 0
-    },
-    stories: {
-        available: 0, 
-        collectionURI: '', 
-        items: [], 
-        returned: 0
-    },
-    thumbnail: {
-        path: '', 
-        extension: ''
-    },
-    urls : []
-}
-
-  public comics    : any=[];
-  public id        : number;
-
-  public checking  = true;
-
-  constructor(private characterService: CharacterService, 
-              private route: ActivatedRoute) { 
-    
-     this.route.queryParams.subscribe(params => {
-      let id = params['id'];
-
-      if(id){
-        this.characterService.getCharacterById(id).then(character => {
-          this.character = character[0] || this.character;
+    name: "",
+    series: "",
+  }
+  
+  constructor(private characterService: CharacterService, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.character.id = params['id'];
           
+        if(this.character.id){
+        this.characterService.getCharacterById(this.character.id).then(character => {
+        this.character = (character[0] || this.character);      
           if(this.character){
-              this.getComicsByCharacter();
+            this.getComicsByCharacter();
           }
         });
-      }
-    });
+      }  
+    });  
   }
 
   public getComicsByCharacter(){
-    this.checking = true;
-
+    
     this.characterService.getComicsByCharacter(this.character).then((comics) => {
-        this.comics   = comics;
-        this.checking = false;
+      this.comics   = comics;
     });
   }
-
 }
